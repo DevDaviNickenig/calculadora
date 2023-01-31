6a080b7ac3fb45d9f32566d930bf0df38d685da9
Davi Nickenig Cassitas Barbosa <devdavinickenig@gmail.com>
2023-01-31 10:37:13 -0300

    update.js
    mudanças no javascript e botoes adicionando multiplos numeros no display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 class Calculadora {
-    constructor(numeroAnterior, numeroAtual) {
-        this.numeroAnterior = numeroAnterior
-        this.numeroAtual = numeroAtual
+    constructor(numeroAnteriorDisplay, numeroAtualDisplay) {
+        this.numeroAnteriorDisplay = numeroAnteriorDisplay
+        this.numeroAtualDisplay = numeroAtualDisplay
+        this.limparTudo()
     }
 
     limparTudo() {
-        this.numeroAn = ''
-        this.numeroAt = ''
+        this.numeroAnterior = ''
+        this.numeroAtual = ''
+
     }
 
     // deletar() {
@@ -14,32 +16,31 @@ class Calculadora {
     // }
 
     apensarValor(numero) {
-        this.numeroAt = numero.toString()
+        this.numeroAtual = this.numeroAtual.toString() + numero.toString()
     }
-
     // operacao() {
 
     // }
 
     atualizarDisplay() {
-        this.numeroAtual.innerText = this.numeroAt
+        this.numeroAtualDisplay.innerText = this.numeroAtual
     }
 
 }
 
 const botoesNumeros = document.querySelectorAll('[data-numeros]')
 const botoesOperadores = document.querySelectorAll('[data-operador]')
-const numeroAnterior = document.querySelector('[data-numero-anterior]')
-const numeroAtual = document.querySelector('[data-numero-atual]')
+const numeroAnteriorDisplay = document.querySelector('[data-numero-anterior]')
+const numeroAtualDisplay = document.querySelector('[data-numero-atual]')
 const botaoLimparTudo = document.querySelector('[data-reset]')
 const botaoDeletar = document.querySelector('[data-delete]')
 const botaoIgual = document.querySelector('[data-igual]')
 
-const calculadora = new Calculadora(numeroAnterior, numeroAtual)
+const calculadora = new Calculadora(numeroAnteriorDisplay, numeroAtualDisplay)
 
-botoesNumeros.forEach(teclas => {
-    teclas.addEventListener('click', e => {
-        calculadora.apensarValor(teclas.innerText)
+botoesNumeros.forEach(botoes => {
+    botoes.addEventListener('click', () => {
+        calculadora.apensarValor(botoes.innerText)
         calculadora.atualizarDisplay()
     })
 })
@@ -65,34 +66,34 @@ botaoIgual.addEventListener('click', e => {
 
 
 
-function calculo() {
-    let calculo
-    const n1 = parseFloat(this.numeroAnterior)
-    const n2 = parseFloat(this.numeroAtual)
-    switch (operador) {
-        case 'AC':
-            displayCalc.innerText = '';
-            break
-        // case 'C'
-        //     displayCalc.innerText = ;
-        // break
-        case '+':
-            calculo = n1 + n2
-            break
-        case '-':
-            calculo = n1 - n2
-            break
-        case '/':
-            //function subtração
-            break
-        case '*':
-            //function multiplicação
-            break
-        default:
-            displayCalc.innerText += e.target.innerText;
-    }
+// function calculo() {
+//     let calculo
+//     const n1 = parseFloat(this.numeroAnterior)
+//     const n2 = parseFloat(this.numeroAtual)
+//     switch (operador) {
+//         case 'AC':
+//             displayCalc.innerText = '';
+//             break
+//         // case 'C'
+//         //     displayCalc.innerText = ;
+//         // break
+//         case '+':
+//             calculo = n1 + n2
+//             break
+//         case '-':
+//             calculo = n1 - n2
+//             break
+//         case '/':
+//             //function subtração
+//             break
+//         case '*':
+//             //function multiplicação
+//             break
+//         default:
+//             displayCalc.innerText += e.target.innerText;
+//     }
 
-}
+// }
 
 // botoes.forEach(teclas => {
 //     teclas.addEventListener('click', e => {
